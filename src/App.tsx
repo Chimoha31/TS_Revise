83a5c750abfee585b816527d55faea9f53e97610
Chimoha31 <mokochii1108@gmail.com>
2022-03-30 10:06:14 -0700

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import "./App.css";
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
-  // Appないで使う型を先に定義しておくと余計なエラーが出なくて楽。
+
+//-------------------------------------------------------------------
+  // App内で使う型を先に定義しておくと余計なエラーが出なくて楽。
   type Todo = {
     inputValue: string;
     id: number;
     checked: boolean;
   };
+//-------------------------------------------------------------------
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -17,12 +20,14 @@ function App() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // 新しいTodoを作成
+ 
+    //  新しいTodoを作成
     const newTodo: Todo = {
       inputValue: inputValue,
       id: todos.length,
       checked: false,
     };
+ 
     setTodos([...todos, newTodo]);
     // ↑元のtodosに対して、newTodosがどんどん入っていく
     setInputValue("");
@@ -30,7 +35,7 @@ function App() {
 
   const handleEdit = (id: number, inputValue: string) => {
     const newTodos = todos.map((todo) => {
-      if(todo.id === id) {
+      if (todo.id === id) {
         todo.inputValue = inputValue;
       }
       return todo;
@@ -38,9 +43,24 @@ function App() {
     setTodos(newTodos);
   };
 
-  const handleChecked = (id:number, checked:boolean) => {
-    
-  }
+  const handleChecked = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => {
+      if (todo.id !== id) {
+        return todo;
+      }
+    });
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
@@ -67,15 +87,26 @@ function App() {
             <li key={todo.id}>
               <input
                 type="text"
-                onChange={(e) => {handleEdit(todo.id, e.target.value)}}
+                onChange={(e) => {
+                  handleEdit(todo.id, e.target.value);
+                }}
                 className="inputText"
                 value={todo.inputValue}
                 disabled={todo.checked}
               />
               <input
                 type="checkbox"
-                onChange={(e) => {handleChecked(todo.id, todo.checked)}}
+                onChange={(e) => {
+                  handleChecked(todo.id, todo.checked);
+                }}
               />
+              <button
+                onClick={() => {
+                  handleDelete(todo.id);
+                }}
+              >
+                Delete
+              </button>
             </li>
           ))}
         </ul>
