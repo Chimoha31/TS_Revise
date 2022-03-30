1539994707180c115863d61223f1913c3f6f70a4
Chimoha31 <mokochii1108@gmail.com>
2022-03-30 09:45:53 -0700

    Edit todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,84 @@
-import React,{ useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
-// Appないで使う型を先に定義しておくと余計なエラーが出なくて楽。
-type Todo = {
-  inputValue: string;
-  id: number;
-  checked: boolean;
-}
+  // Appないで使う型を先に定義しておくと余計なエラーが出なくて楽。
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
 
-const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setInputValue(e.target.value);
-}
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
 
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  // 新しいTodoを作成
-  const newTodo: Todo = {
-    inputValue: inputValue,
-    id: todos.length,
-    checked: false,
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+    setTodos([...todos, newTodo]);
+    // ↑元のtodosに対して、newTodosがどんどん入っていく
+    setInputValue("");
+  };
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleChecked = (id:number, checked:boolean) => {
+    
   }
-  setTodos([newTodo, ...todos]);
-  // ↑元のtodosに対して、newTodosがどんどん入っていく
-}
 
   return (
     <div className="App">
       <div>
         <h2>Todo List with Typescript</h2>
-        <form onSubmit={(e) => {handleSubmit(e)}}>
-          <input type="text" onChange={(e) => {handleChange(e)}} className="inputText"></input>
-          <input type="submit" value="Create" className="submitButton"></input>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className="inputText"
+          />
+          <input type="submit" value="Create" className="submitButton" />
         </form>
+
+        {/* inputに入力したtodoをuiに反映させる */}
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="text"
+                onChange={(e) => {handleEdit(todo.id, e.target.value)}}
+                className="inputText"
+                value={todo.inputValue}
+                disabled={todo.checked}
+              />
+              <input
+                type="checkbox"
+                onChange={(e) => {handleChecked(todo.id, todo.checked)}}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
